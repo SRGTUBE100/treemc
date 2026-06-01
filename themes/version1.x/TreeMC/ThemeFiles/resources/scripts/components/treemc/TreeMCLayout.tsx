f018df6b864befb6dd4a4c1e7e5c1c09b5f9f6fe
SRGTUBE100 <ramnareshrathour1001@gmail.com>
2026-06-01 18:37:27 +0530

    Update TreeMCLayout.tsx

--- themes/version1.x/TreeMC/ThemeFiles/resources/scripts/components/treemc/TreeMCLayout.tsx
+++ themes/version1.x/TreeMC/ThemeFiles/resources/scripts/components/treemc/TreeMCLayout.tsx
@@ -1,19 +1,37 @@
 import React from 'react';
+import TreeMCSidebar from './TreeMCSidebar';
 
 interface Props {
-children: React.ReactNode;
+    children: React.ReactNode;
 }
 
 export default ({ children }: Props) => {
-return (
-<div
-style={{
-minHeight: '100vh',
-padding: '30px',
-maxWidth: '1600px',
-margin: '0 auto',
-}}
->
-{children} </div>
-);
+    return (
+        <div
+            style={{
+                minHeight: '100vh',
+
+                backgroundImage:
+                    "linear-gradient(rgba(3,7,18,.85), rgba(3,7,18,.92)), url('https://media.craiyon.com/2025-07-23/OAnQPJ7TSlS8qHvUXtugkQ.webp')",
+
+                backgroundSize: 'cover',
+                backgroundPosition: 'center',
+                backgroundAttachment: 'fixed',
+
+                display: 'flex',
+            }}
+        >
+            <TreeMCSidebar />
+
+            <div
+                style={{
+                    flex: 1,
+                    padding: '30px',
+                    overflowY: 'auto',
+                }}
+            >
+                {children}
+            </div>
+        </div>
+    );
 };
