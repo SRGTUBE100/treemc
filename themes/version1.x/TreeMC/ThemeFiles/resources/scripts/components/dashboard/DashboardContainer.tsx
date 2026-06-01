9f8d925c8d2d4b7dd6773a71084b13a0bc239861
SRGTUBE100 <ramnareshrathour1001@gmail.com>
2026-06-01 17:44:25 +0530

    Update DashboardContainer.tsx

--- themes/version1.x/TreeMC/ThemeFiles/resources/scripts/components/dashboard/DashboardContainer.tsx
+++ themes/version1.x/TreeMC/ThemeFiles/resources/scripts/components/dashboard/DashboardContainer.tsx
@@ -1,11 +1,36 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Server } from '@/api/server/getServer';
+import getServers from '@/api/getServers';
+import ServerRow from '@/components/dashboard/ServerRow';
 import PageContentBlock from '@/components/elements/PageContentBlock';
+import Spinner from '@/components/elements/Spinner';
+import useSWR from 'swr';
+import { PaginatedResult } from '@/api/http';
+
 import TreeMCDashboard from './TreeMCDashboard';
 
 export default () => {
+    const [page] = useState(1);
+
+    const { data: servers } = useSWR<PaginatedResult<Server>>(
+        ['/api/client/servers', page],
+        () => getServers({ page })
+    );
+
     return (
         <PageContentBlock title={'Dashboard'}>
-            <TreeMCDashboard />
+            {!servers ? (
+                <Spinner centered size={'large'} />
+            ) : (
+                <TreeMCDashboard serverCount={servers.items.length}>
+                    {servers.items.map((server) => (
+                        <ServerRow
+                            key={server.uuid}
+                            server={server}
+                        />
+                    ))}
+                </TreeMCDashboard>
+            )}
         </PageContentBlock>
     );
 };
