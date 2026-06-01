7347ec0ee5ea9b1ca602efd33e71cbe368e8c5ce
SRGTUBE100 <ramnareshrathour1001@gmail.com>
2026-06-01 17:43:29 +0530

    Update TreeMCStats.tsx

--- themes/version1.x/TreeMC/ThemeFiles/resources/scripts/components/dashboard/TreeMCStats.tsx
+++ themes/version1.x/TreeMC/ThemeFiles/resources/scripts/components/dashboard/TreeMCStats.tsx
@@ -1,41 +1,43 @@
 import React from 'react';
 import StatCard from '../treemc/StatCard';
 
-export default () => {
-return (
-<div
-style={{
-display: 'grid',
-gridTemplateColumns: 'repeat(4,1fr)',
-gap: '20px',
-marginTop: '25px',
-}}
-> <StatCard
-             title="Servers"
-             value="0"
-             subtitle="Online Servers"
-         />
+interface Props {
+    serverCount: number;
+}
 
+export default ({ serverCount }: Props) => {
+    return (
+        <div
+            style={{
+                display: 'grid',
+                gridTemplateColumns: 'repeat(4,1fr)',
+                gap: '20px',
+                marginTop: '25px',
+            }}
+        >
+            <StatCard
+                title="Servers"
+                value={String(serverCount)}
+                subtitle="Your Servers"
+            />
 
-        <StatCard
-            title="Players"
-            value="0"
-            subtitle="Connected Players"
-        />
+            <StatCard
+                title="Players"
+                value="0"
+                subtitle="Connected Players"
+            />
 
-        <StatCard
-            title="CPU Usage"
-            value="0%"
-            subtitle="All Nodes"
-        />
+            <StatCard
+                title="CPU Usage"
+                value="0%"
+                subtitle="All Nodes"
+            />
 
-        <StatCard
-            title="RAM Usage"
-            value="0%"
-            subtitle="All Nodes"
-        />
-    </div>
-);
-
-
+            <StatCard
+                title="RAM Usage"
+                value="0%"
+                subtitle="All Nodes"
+            />
+        </div>
+    );
 };
